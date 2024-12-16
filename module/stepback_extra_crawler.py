8de8ceb098906ef12e963736afb154fdf42a56fd
LoyAngel <loyangel@proton.me>
2024-12-16 10:39:58 +0800

    stepback extra add: add new method to optimize results

--- module/stepback_extra_crawler.py
+++ module/stepback_extra_crawler.py
@@ -1,6 +1,6 @@
 import json, re
 from lxml import etree, html
-from utils.html_utils import simplify_html, find_common_ancestor, get_absolute_xpath
+from utils.html_utils import simplify_html, find_common_ancestor, get_pure_text, html_element_text_similarity_cal
 from utils.step import domlm_parse
 from bs4 import BeautifulSoup
 
@@ -20,6 +20,13 @@ Please output in the following Json format:
     "value": "", # the value extracted from the HTML that match the instruction, if there is no data, keep it blank
     "xpath": "" # a workable xpath to extract the value in the HTML
 }}
+
+Please think step by step as following:
+0. Find the target value suiting the instruction above first, and make it be the value in the following json.
+1. Find generalizable text attributes related to the values specified in the instruction using class names, text(), and descendant element content. Construct a base XPath from these attributes, ensuring it is as unique as possible. 
+2. Because the unique element has been located by relative positioning, the next step is to extend the base Xpath using absolute positioning. Try to use the '[NUM]' to locate the element.'text()' and 'following-sibling' could aslo be used in the xpath. Ensure the final XPath selector returns a unique element.
+3. Write the result in the Json format as above.
+
 Here's the HTML code:
 ```
 {1}
@@ -64,7 +71,8 @@ Please output your judgement in the following Json format:
 
 judgement_prompt = '''Your main task is to judge whether the extracted value is consistent with the expected value, which is recognized beforehand. Please pay attention for the following case:
     1) If the extracted result contains some elements that is not in expected value, or contains empty value, it is not consistent.
-    2) The raw values containing redundant separators is considered as consistent because we can postprocess it.
+    2) Differences in punctuations, symbols or formatting are acceptable.
+    3) The following cases are considered matching: Type differences ; Repeated values ; Values with prefix.
 
 The extracted value is: {0}
 The expected value is: {1}
@@ -76,19 +84,45 @@ Please output your judgement in the following Json format:
 }}
 '''
 
-synthesis_prompt = '''You're a perfect discriminator which is good at HTML understanding as well. Following the instruction, there are some action sequence written from several HTML and the corresponding result extracted from several HTML. Please choose one that can be best potentially adapted to the same extraction task on other webpage in the same websites. Here are the instruction of the task:
+synthesis_prompt = '''You're a perfect discriminator which is good at HTML understanding as well. Following the instruction, there are some action sequence written from several HTML and the corresponding result extracted from several HTML. Please choose one that can be best potentially adapted to the same extraction task on other webpage in the same websites.
+Please evaluate each action sequence based on these criteria, rating them on a scale of 1-10 (integer):
+Accuracy: Whether the value in the "extracted result" matches the content required by the instructions.
+Generalizability: The preference for flexible selectors (e.g., [@class='st.']) over rigid ones (e.g., [text()='st.']), indicating better adaptability.
+Simplicity: Shorter expressions are preferred over longer ones, provided the output remains stable and accurate.
+
+Here are the instruction of the task:
 Instructions: {0}
+
+Pay Attention:
+The action sequences are not alternative paths - all steps must be executed in sequence to retrieve the value.
+
 The action sequences and the corresponding extracted results with different sequence on different webpage are as follow:
 {1}
 
-Please output the best action sequence in the following Json format:
+
+Please rate every action sequence in the following Json format:
 {{
-    "thought": "" # brief thinking about which to choose
-    "number": "" # the best action sequence choosen from the candidates, starts from 0. If there is none, output 0.
+    "thought": "" # brief explanation of your selection rationale.
+    "rate": "" # list of ratings for each sequence, formatted as "[[accuracy1, generalizability1, robustness1], ...]", a null action sequence should be rated by "[0, 0, 0]".
 }}
 '''
 
-class StepbackCrawler:
+inverted_prompt = '''You're a perfect text reader which is good at understanding the text content. Please recognize the best value following the instruction below.
+
+Instruction: {0}
+Please output in the following Json format:
+{{
+    "thought": "", # a brief thought of how to confirm the value
+    "value": "" # the value extracted from the text content that match the instruction
+}}
+
+Here's the text content:
+```
+{1}
+```
+
+'''
+class StepbackExtraCrawler:
     def __init__(self,
                  simplify=True,
                  verbose=True,
@@ -139,10 +173,11 @@ class StepbackCrawler:
                           instruction: str,
                           html_content: str,
                           ground_truth=None):
+        LOOP_TIMES = 3
 
         action_sequence = []
 
-        for _ in range(3):
+        for index in range(LOOP_TIMES):
             if ground_truth:
                 query = f'{role_prompt}{crawler_wr_prompt.format(instruction, ground_truth, html_content)}'
                 res = self.request_parse(query, ['thought', 'xpath'])
@@ -156,49 +191,54 @@ class StepbackCrawler:
             xpath = res['xpath']
             value = ground_truth if ground_truth else res['value']
         
-
+            
             try:
                 print('-' * 50)
                 print(value)
                 print(results)
                 print(xpath)
-            except Exception as e:
-                print("pass")
+            except:
+                pass
 
             if value == '':
                 return action_sequence
 
-            query = f'{role_prompt}/n{judgement_prompt.format(str(results), value)}'
-
+            query = f'{role_prompt}\n{judgement_prompt.format(str(results), value)}'
             res = self.request_parse(query, ['thought', 'judgement'])
-
-            print(json.dumps(res, ensure_ascii=False, indent=4))
             if res['judgement'].lower() == 'yes':
                 action_sequence.append(xpath)
                 return action_sequence
+
+            if index == LOOP_TIMES - 1: # Last loop doesn't need to stepback
+                break
             
             # Stepback
             while True:
                 new_html_content = find_common_ancestor(html_content, xpath)
-                query = f'{role_prompt}/n{stepback_prompt.format(instruction, value, new_html_content)}'
-                res = self.request_parse(query, ['thought', 'judgement'])
-                if res['judgement'] == 'yes' or new_html_content == html_content:
+                new_html_content_clear = re.sub(r'\s', '', new_html_content)
+                # query = f'{role_prompt}\n{stepback_prompt.format(instruction, value, new_html_content)}'
+                # res = self.request_parse(query, ['thought', 'judgement'])
+                # if res['judgement'] == 'yes' or new_html_content == html_content:
+                #     action_sequence.append(xpath)
+                #     break
+                # else:
+                #     xpath += '/..'
+
+                is_step_back = False
+                values = value.split(',') if ',' in value else [value]
+                values = [re.sub(r'\s', '', value) for value in values]
+                for value in values:
+                    if value not in new_html_content_clear:
+                        is_step_back = True
+                        break
+                if is_step_back and new_html_content != html_content:
+                    xpath += '/..'
+                else:
                     action_sequence.append(xpath)
                     break
-                else:
-                    xpath += '/..'
-
-            # Forward
-            # while True:
-            #     new_html_content = find_common_ancestor(html_content, xpath)
-            #     query = f'{role_prompt}/n{stepback_prompt.format(instruction, value, new_html_content)}'
-            #     res = self.request_parse(query, ['thought', 'judgement'])
-            #     if res['judgement'] == 'yes':
-            #         action_sequence.append(xpath)
-            #         break
-            #     else:
-            #         xpath += '/..'
+                    
             html_content = new_html_content
+
         return action_sequence
 
     def generate_sequence(self, instruction, html_content, ground_truth = None, max_token=8000):
@@ -226,14 +266,102 @@ class StepbackCrawler:
                 sub_extract_result['extracted result'] = self.extract_with_sequence(html_content, rule)
                 extract_result.append(sub_extract_result)
             print(json.dumps(extract_result, ensure_ascii=False, indent=4))
-            query = synthesis_prompt.format(instruction, json.dumps(extract_result, indent=4))
-            res = self.request_parse(query, ['thought', 'number'])
-            try:
-                return rule_list[eval(res['number'])]
-            except:
-                return rule_list[0]
+            return self.rate_and_perferred(instruction, extract_result, rule_list)
         else:
             return rule_list[0]
+        
+    def rate_and_perferred(self, instruction, extract_result, rule_list):
+        ACCURACY_SCORE_WEIGHT = 0.4
+        GENERALIZABILITY_SCORE_WEIGHT = 0.2
+        ACTION_COUNT_SCORE_WEIGHT = 0.15
+        AVERAGE_LENGTH_SCORE_WEIGHT = 0.15
+        ROBUSTNESS_SCORE_WEIGHT = 0.1
+
+        def local_rate():
+            # action count score
+            action_count_list = [len(rule) for rule in rule_list]
+            action_count_max, action_count_min = max(action_count_list), min(action_count_list)
+            if action_count_max == action_count_min:
+                action_count_score_list = [10] * len(action_count_list)
+            else:
+                action_count_score_list = [
+                    int(10 - 9 * (action_count_score - action_count_min) / (action_count_max - action_count_min)) 
+                    for action_count_score in action_count_list
+                ]
+
+            # average length score
+            average_length_list = []
+            for rule in rule_list:
+                rule_total_length = 0
+                for xpath in rule:
+                    rule_total_length += len(xpath)
+                if rule_total_length == 0:
+                    average_length_list.append(0)
+                else:
+                    average_length_list.append(rule_total_length / len(rule))
+            average_length_max, average_length_min = max(average_length_list), min(average_length_list)
+            if average_length_max == average_length_min:
+                average_length_score_list = [10] * len(average_length_list)
+            else:
+                average_length_score_list = [
+                    int(10 - 9 * (average_length_score - average_length_min) / (average_length_max - average_length_min))
+                    for average_length_score in average_length_list
+                ]
+
+            return smooth_scores(action_count_score_list), smooth_scores(average_length_score_list)
+
+        def smooth_scores(score_list):
+            '''
+            Smooth the scores process like LLM does
+            '''
+            sorted_indices = sorted(range(len(score_list)), key=lambda k: score_list[k])
+            sorted_scores = [score_list[i] for i in sorted_indices]
+            length = len(score_list)
+            
+            # Generate smoothed scores
+            if length < 10:
+                smoothed_scores = list(range(10, 10 - length, -1))
+            else:
+                smoothed_scores = [10 - int(10 * i / length) for i in range(length)]
+            
+            # Assign smoothed scores, ensuring equal original scores get the same smoothed score
+            final_scores = [0] * length
+            last_score, last_smoothed = None, None
+            
+            for i, index in enumerate(sorted_indices):
+                if sorted_scores[i] == last_score:
+                    final_scores[index] = last_smoothed
+                else:
+                    last_score = sorted_scores[i]
+                    last_smoothed = smoothed_scores[i]
+                    final_scores[index] = last_smoothed
+            
+            return final_scores
+    
+        query = synthesis_prompt.format(instruction, json.dumps(extract_result, indent=4))
+        res = self.request_parse(query, ['thought', 'rate'])
+        llm_rate = eval(res['rate']) if isinstance(res['rate'], str) else res['rate']
+        action_count_score_list, average_length_score_list = local_rate()
+        
+        total_score_list = []
+        final_index = 0
+        try:
+            for index in range(len(rule_list)):
+                total_score = action_count_score_list[index] * ACTION_COUNT_SCORE_WEIGHT + \
+                                average_length_score_list[index] * AVERAGE_LENGTH_SCORE_WEIGHT + \
+                                llm_rate[index][0] * ACCURACY_SCORE_WEIGHT + \
+                                llm_rate[index][1] * GENERALIZABILITY_SCORE_WEIGHT + \
+                                llm_rate[index][2] * ROBUSTNESS_SCORE_WEIGHT
+                total_score_list.append(total_score)
+            print(total_score_list)
+            final_index = total_score_list.index(max(total_score_list))
+        except:
+            import traceback
+            traceback.print_exc()
+            print(llm_rate)
+
+        print(f"Final index: {final_index}")
+        return rule_list[final_index]
 
     def rule_synthesis(self, 
                        website_name: str,
@@ -251,11 +379,16 @@ class StepbackCrawler:
                 rule_list.append(page_rule)
         else:
             for html_content in seed_html_set:
-                page_rule = self.generate_sequence(instruction, html_content, max_token=max_token)
+                inverted_html_content = self.inverted_search(html_content, instruction)
+                page_rule = self.generate_sequence(instruction, inverted_html_content, max_token=max_token)
                 rule_list.append(page_rule)
 
         #rule_list = list(set(rule_list))
-        print(rule_list)
+        try:
+            print(rule_list)
+        except:
+            pass
+
         if len(seed_html_set) > 1:
             valid_answer = False
             for rule in rule_list:
@@ -269,66 +402,17 @@ class StepbackCrawler:
                 for html_content in seed_html_set:
                     sub_extract_result['extracted result'].append(self.extract_with_sequence(html_content, rule))
                 extract_result.append(sub_extract_result)
-            print('+' * 100)
-            print(f"Systhesis rule for the website {website_name}")
+            
             try:
+                print('+' * 100)
+                print(f"Systhesis rule for the website {website_name}")
                 print(json.dumps(extract_result, ensure_ascii=False, indent=4))
             except:
-                print('pass')
-            query = synthesis_prompt.format(instruction, json.dumps(extract_result, indent=4))
-            res = self.request_parse(query, ['thought', 'number'])
-            try:
-                return rule_list[eval(res['number'])]
-            except:
-                return rule_list[0]
+                pass
+
+            return self.rate_and_perferred(instruction, extract_result, rule_list)
         else:
             return rule_list[0]
-        
-    def rule_synthesis_cul(self, 
-                       website_name: str,
-                       seed_html_set: list[str], 
-                       instruction: str, 
-                       ground_truth = None,
-                       max_token = 8000,
-                       per_page_repeat_time=1):
-        rule_list_set = []
-        rule_list = []
-
-        # Collect a rule from each seed webpage
-        if ground_truth:
-            for html_content, gt in zip(seed_html_set, ground_truth):
-                page_rule = self.generate_sequence(instruction, html_content, gt, max_token=max_token)
-                rule_list.append(page_rule)
-        else:
-            for html_content in seed_html_set:
-                page_rule = self.generate_sequence(instruction, html_content, max_token=max_token)
-                rule_list.append(page_rule)
-
-        #rule_list = list(set(rule_list))
-        print(rule_list)
-        for webnum in range(2, len(seed_html_set) + 1):
-            valid_answer = False
-            for rule in rule_list[:webnum]:
-                if rule != []:
-                    valid_answer = True
-            if not valid_answer:
-                return []
-            extract_result = []
-            for rule in rule_list[:webnum]:
-                sub_extract_result = {'rule':rule, 'extracted result':[]}
-                for html_content in seed_html_set[:webnum]:
-                    sub_extract_result['extracted result'].append(self.extract_with_sequence(html_content, rule))
-                extract_result.append(sub_extract_result)
-            print('+' * 100)
-            print(f"Systhesis rule for the website {website_name}")
-            print(json.dumps(extract_result, ensure_ascii=False, indent=4))
-            query = synthesis_prompt.format(instruction, json.dumps(extract_result, indent=4))
-            res = self.request_parse(query, ['thought', 'number'])
-            try:
-                rule_list_set.append(rule_list[eval(res['number'])])
-            except:
-                rule_list_set.append(rule_list[0])
-        return rule_list_set
         
     def extract_with_xpath(self, 
                            html_content:str, 
@@ -370,3 +454,18 @@ class StepbackCrawler:
                         pass
                 else:
                     return self.extract_with_xpath(html_content, xpath)
+    
+    def inverted_search(self, html_content:str, instruction:str):
+        if self.is_simplify:
+            html_content = simplify_html(html_content)
+        query = f'{role_prompt}\n{inverted_prompt.format(instruction, get_pure_text(html_content))}'
+        res = self.request_parse(query, ['thought', 'value'])
+        print(res)
+        value = res['value']
+
+        return html_element_text_similarity_cal(value, html_content)
+
+
+
+       
+        
--- utils/html_utils.py
+++ utils/html_utils.py
@@ -5,6 +5,7 @@ from lxml import etree
 from lxml.etree import _Element
 import bs4
 import tiktoken
+import difflib
 
 class StepTree:
     def __init__(self, html, step_len = 5000, len_func = len):
@@ -23,15 +24,15 @@ def find_common_ancestor(html_content:str, xpath:str):
         xpath = xpath.replace('::text()','::*')
         xpath = xpath.replace('/text()','/*')
         nodes = tree.xpath(xpath)
-        # 获取每个节点的所有祖先节点
+        # get the ancestors of each node
         ancestors_list = [set(n.xpath('ancestor::*')) for n in nodes]
 
         if not ancestors_list:
             return html_content
-        # 找到所有祖先集合的交集，即共同祖先
+        # find the common ancestors
         common_ancestors = set.intersection(*ancestors_list)
 
-        # 选择最近的共同祖先（即最后一个共同祖先）
+        # choose the nearest common ancestor (the one with the longest path)
         nearest_common_ancestor = max(common_ancestors, key=lambda x: x.getroottree().getpath(x).count('/'))
         ancestor_string = etree.tostring(nearest_common_ancestor, pretty_print=True, encoding='unicode')
         return ancestor_string
@@ -78,6 +79,8 @@ def simplify_html(html, reserve_attrs = ['class']):
         element.extract()
     [s.extract() for s in soup('script')]
     [s.extract() for s in soup('style')]
+    [s.extract() for s in soup('img')]
+    [s.extract() for s in soup('input')]
     for tag in soup.find_all():
         try:
             new_attrs = {}
@@ -88,6 +91,9 @@ def simplify_html(html, reserve_attrs = ['class']):
         except:
             pass
     html = str(soup)
+    
+    html = '\n'.join([line for line in html.split('\n') if line.strip() != ''])
+
     return html
 
 def parse_accessibility_tree(html, indent='\t'):
@@ -109,6 +115,63 @@ def num_tokens_from_string(string: str, encoding_name: str) -> int:
     #print(string)
     num_tokens = len(encoding.encode(string))
     return num_tokens
+
+def get_pure_text(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    text = soup.get_text().replace('\t',' ')
+    text = '\n'.join([line for line in text.split('\n') if line.strip() != ''])
+    text = ' '.join([line for line in text.split(' ') if line.strip() != ''])
+    return text
+
+def html_element_text_similarity_cal(text, html):
+    LEVEL_NUM = 3 # the number of ancestor levels to consider
+    COMBINED_NUM = 3 
+    def similarity_cal(text1, text2) -> float:
+        # calculate the similarity between two strings
+        seq = difflib.SequenceMatcher(None, text1, text2)
+        return seq.ratio()
+    
+    if text == "":
+        return html
+
+
+    # extract the text of each element in the html, excluding the text of its children
+    element_texts = []
+    tree = etree.HTML(html)
+    for element in tree.iter():
+        if element.text:
+            element_texts.append(element.text)
+        if element.tail:
+            element_texts.append(element.tail)
+    
+    # calculate the similarity between the given text and the text of each element 
+    similarities = [similarity_cal(text, element_text) for element_text in element_texts]
+    sorted_similarities = sorted(similarities, reverse=True)
+
+    combined_html = ""
+    for index in range(COMBINED_NUM):
+        similar_element_text = element_texts[similarities.index(sorted_similarities[index])]
+        try:
+            print(f'similar_element{index}:{similar_element_text}')
+        except:
+            pass
+        # find the common ancestor of the most similar element
+        soup = BeautifulSoup(html, 'html.parser')
+        element = soup.find(string=similar_element_text)
+        
+        # element's N-level ancestor
+        ancestor = element
+        if ancestor:
+            for _ in range(LEVEL_NUM):
+                if ancestor.parent:
+                    ancestor = ancestor.parent
+                else:
+                    break
+        
+        combined_html += str(ancestor)
+    
+    return combined_html
+
 
 if __name__ == '__main__':
     import requests
